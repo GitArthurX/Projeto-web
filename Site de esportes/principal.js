b383a69e42152620dc34e9711f962be54b59a384
GitArthurX <arthurzanquetin13@gmail.com>
2024-04-08 16:11:10 -0300

    Postagem do projeto
    postei o meu projeto!

---
+++ Site de esportes/principal.js
@@ -0,0 +1,63 @@
+document.getElementById("redirecionarFutebol1").addEventListener("click", function() {
+    window.location.href = "futebol.html#futebol1";
+});
+
+document.getElementById("redirecionarBasquete1").addEventListener("click", function() {
+    window.location.href = "basquete.html#basquete1";
+});
+
+document.getElementById("redirecionareSports1").addEventListener("click", function() {
+    window.location.href = "eSports.html#eSports1";
+});
+
+document.getElementById("redirecionarFutebol4").addEventListener("click", function() {
+    window.location.href = "futebol.html#Futebol4";
+});
+
+document.getElementById("redirecionarFutebol2").addEventListener("click", function() {
+    window.location.href = "futebol.html#futebol2";
+});
+
+document.getElementById("redirecionar_Futebol_2").addEventListener("click", function() {
+    window.location.href = "futebol.html#futebol2";
+});
+
+document.getElementById("redirecionarFutebol3").addEventListener("click", function() {
+    window.location.href = "futebol.html#futebol3";
+});
+
+document.getElementById("redirecionar_Futebol_3").addEventListener("click", function() {
+    window.location.href = "futebol.html#futebol3";
+});
+
+document.getElementById("redirecionarBasquete2").addEventListener("click", function() {
+    window.location.href = "basquete.html#basquete2";
+});
+
+document.getElementById("redirecionar_Basquete_2").addEventListener("click", function() {
+    window.location.href = "basquete.html#basquete2";
+});
+
+document.getElementById("redirecionarBasquete3").addEventListener("click", function() {
+    window.location.href = "basquete.html#basquete3";
+});
+
+document.getElementById("redirecionar_Basquete_3").addEventListener("click", function() {
+    window.location.href = "basquete.html#basquete3";
+});
+
+document.getElementById("redirecionareSports2").addEventListener("click", function() {
+    window.location.href = "eSports.html#eSports2";
+});
+
+document.getElementById("redirecionar_eSports_2").addEventListener("click", function() {
+    window.location.href = "eSports.html#eSports2";
+});
+
+document.getElementById("redirecionareSports3").addEventListener("click", function() {
+    window.location.href = "eSports.html#eSports3";
+});
+
+document.getElementById("redirecionar_eSports_3").addEventListener("click", function() {
+    window.location.href = "eSports.html#eSports3";
+});
